5331424e88ca74abb8414d76673d723131c036f4
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2021-10-01 11:17:15 +0530

    Added register/log php files

---
+++ backend/register.php
@@ -0,0 +1,31 @@
+<?php 
+	
+	require_once('dbconnection.php');
+
+
+ ?>
+
+ <?php 
+ 	session_start();
+ 	//echo $_SESSION['name'];
+ 	
+ 	if(isset($_POST['submit'])){
+ 		$name = $_POST['name'];
+        $shop_name = $_POST['shop_name'];
+ 		$email = $_POST['email'];
+ 		$password = $_POST['password'];
+ 		
+ 		//create query
+ 		$query = "INSERT INTO students (name,shop_name,email,password) VALUES ('{$name}','{$shop_name}','{$email}','{$password}')";
+ 		//execute query
+ 		$result = mysqli_query($connect,$query);
+ 			//redirect page
+ 		if($result){
+ 			header('Location: log.php');
+ 		}else{
+ 			echo "weradi";
+ 		}
+ 	}
+
+
+  ?>
